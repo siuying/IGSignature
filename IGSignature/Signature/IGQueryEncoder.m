1971ea5b3fdac29d9ec433fac307baaf30400f8b
Cheng Yao <kukathe@gmail.com>
2014-12-04 17:42:45 +0800

    Add nested NSArray/NSSet/NSDictionary support

--- IGSignature/Signature/IGQueryEncoder.m
+++ IGSignature/Signature/IGQueryEncoder.m
@@ -11,12 +11,34 @@
 @implementation IGQueryEncoder
 
 +(NSString*) encodeParamWithoutEscapingUsingKey:(NSString*)key andValue:(id<NSObject>)value {
+    
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"description" ascending:YES selector:@selector(compare:)];
+    
     if ([value isKindOfClass:[NSArray class]]) {
         NSArray* array = (NSArray*) value;
         NSMutableArray* encodedArray = [NSMutableArray array];
-        [array enumerateObjectsUsingBlock:^(id<NSObject> obj, NSUInteger idx, BOOL *stop) {
-            [encodedArray addObject:[NSString stringWithFormat:@"%@[]=%@", key, obj]];
+        NSArray *sortedArray = [array sortedArrayUsingDescriptors:@[ sortDescriptor ]];
+        [sortedArray enumerateObjectsUsingBlock:^(id<NSObject> obj, NSUInteger idx, BOOL *stop) {
+            NSString *nestedKey = [NSString stringWithFormat:@"%@[]", key];
+            [encodedArray addObject:[self encodeParamWithoutEscapingUsingKey:nestedKey andValue:obj]];
         }];
+        return [encodedArray componentsJoinedByString:@"&"];
+    } else if ([value isKindOfClass:[NSSet class]]) {
+        NSSet *set = (NSSet *) value;
+        NSMutableArray* encodedArray = [NSMutableArray array];
+        NSString *nestedKey = [NSString stringWithFormat:@"%@[]", key];
+        for (id obj in [set sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
+            [encodedArray addObject:[self encodeParamWithoutEscapingUsingKey:nestedKey andValue:obj]];
+        }
+        return [encodedArray componentsJoinedByString:@"&"];
+    } else if ([value isKindOfClass:[NSDictionary  class]]) {
+        NSDictionary *dict = (NSDictionary *) value;
+        NSMutableArray* encodedArray = [NSMutableArray array];
+        for (id subkey in [dict.allKeys sortedArrayUsingDescriptors:@[ sortDescriptor ]]) {
+            id obj = [dict objectForKey:subkey];
+            NSString *nestedKey = [NSString stringWithFormat:@"%@[%@]", key, subkey];
+            [encodedArray addObject:[self encodeParamWithoutEscapingUsingKey:nestedKey andValue:obj]];
+        }
         return [encodedArray componentsJoinedByString:@"&"];
     } else {
         return [NSString stringWithFormat:@"%@=%@", key, value];
